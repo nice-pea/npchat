5feceb6fdd1466db456aac307f8b18440a3200a7
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-05-21 08:35:12 +0300

    refactor: mv shutdown and wg to `app.Start`, rm error from `app.Start` fn

--- cmd/cute-chat-backend/main.go
+++ cmd/cute-chat-backend/main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"context"
 	"github.com/saime-0/cute-chat-backend/internal/app"
 	"github.com/saime-0/cute-chat-backend/internal/config"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
-	"time"
 )
 
 func main() {
-	wg := sync.WaitGroup{}
-	cfg := config.Load()
-	ctx, cancel := context.WithCancel(context.Background())
-
-	app.Start(ctx, wg, cfg)
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-quit
-	go func() {
-		time.Sleep(3 * time.Second)
-		cancel()
-	}()
-	wg.Wait()
+	app.Start(config.Load())
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,17 +2,24 @@ package app
 
 import (
 	"context"
-	"fmt"
 	"github.com/jackc/pgx/v5"
 	"github.com/saime-0/cute-chat-backend/internal/config"
 	"github.com/saime-0/cute-chat-backend/internal/repository/postgres"
+	"log"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 )
 
-func Start(ctx context.Context, wg sync.WaitGroup, cfg *config.Config) error {
+func Start(cfg *config.Config) {
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := new(sync.WaitGroup)
+
 	db, err := pgx.Connect(ctx, cfg.DbConnString())
 	if err != nil {
-		return fmt.Errorf("Unable to connect to database: %v\n", err)
+		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	wg.Add(1)
 	go func() {
@@ -20,7 +27,6 @@ func Start(ctx context.Context, wg sync.WaitGroup, cfg *config.Config) error {
 		db.Close(context.Background())
 		wg.Done()
 	}()
-
 	bus := eventbus.New()
 	//trans := transaction.New[*sqlx.Tx]
 	//customerWrite := postgres.NewCustomerRepository(trans)
@@ -57,4 +63,33 @@ func Start(ctx context.Context, wg sync.WaitGroup, cfg *config.Config) error {
 		return outboxMan.Create(ctx, de)
 	})
 	outboxMan.Start(ctx, lock, cfg.OutboxHeartbeat)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	select {
+	case s := <-interrupt:
+		log.Printf("app - Run - signal: " + s.String())
+		//case err = <-httpServer.Notify():
+		//	l.Error(fmt.Errorf("app - Run - httpServer.Notify: %w", err))
+		//case err = <-rmqServer.Notify():
+		//	l.Error(fmt.Errorf("app - Run - rmqServer.Notify: %w", err))
+	}
+	// use ctx instead call `Shutdown` on each component
+	go func() {
+		time.Sleep(3 * time.Second)
+		cancel()
+	}()
+	wg.Wait()
+
+	// alternative:
+	// call `Shutdown` on each component
+	//err = httpServer.Shutdown()
+	//if err != nil {
+	//	l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
+	//}
+	//
+	//err = rmqServer.Shutdown()
+	//if err != nil {
+	//	l.Error(fmt.Errorf("app - Run - rmqServer.Shutdown: %w", err))
+	//}
 }
