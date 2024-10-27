73a9a783302fccbdfb6cb51921ea24bc7c29d5b1
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-10-27 06:34:45 +0300

    remove commented code

--- android/app/src/main/java/ru/saime/nice_pea_chat/data/store/NpcClientStore.kt
+++ android/app/src/main/java/ru/saime/nice_pea_chat/data/store/NpcClientStore.kt
@@ -8,26 +8,10 @@ class NpcClientStore(context: Context) {
     private val name = "npcClientStore"
     private val sp = context.getSharedPreferences(name, Context.MODE_PRIVATE)
 
-//    private val hostKey = "host"
-//    var host: String
-//        get() = sp.getString(hostKey, "").orEmpty()
-//        set(value) {
-//            sp.edit { putString(hostKey, value) }
-//        }
-//
-//    private val portKey = "port"
-//    var port: Int
-//        get() = sp.getInt(portKey, 0)
-//        set(value) {
-//            sp.edit { putInt(portKey, value) }
-//        }
-
     private val baseUrlKey = "baseUrl"
     var baseUrl: String
         get() = sp.getString(baseUrlKey, "").orEmpty()
         set(value) {
             sp.edit { putString(baseUrlKey, value) }
         }
-
-//    fun urlIsNotEmpty() = host == "" || port == 0
 }
