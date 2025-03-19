b000a3e92285f4939219b9a36a2b1bf8de92d948
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-19 23:06:28 +0300

    добавить комментов в пакет репозитория

--- clean/internal/repository/base/members.go
+++ clean/internal/repository/base/members.go
@@ -1,11 +1,8 @@
 package base
 
-// TODO: раскомментить.
 // Эта строка будет выдавать ошибку во время компиляции,
 // если тип справа не имплементирует интерфейс слева
-//var _ domain.MembersRepository = (*MembersRepository)(nil)
+//var _ domain.MembersRepository = (*MembersRepository)(nil) // TODO: раскомментить.
 
 type MembersRepository struct {
 }
-
-// TODO: реализовать методы
