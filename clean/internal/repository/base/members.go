26c15114e3885432057db6546db1d7047da15797
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-19 22:59:00 +0300

    добавить документацию в пакет репозиториев

--- clean/internal/repository/base/members.go
+++ clean/internal/repository/base/members.go
@@ -1,20 +1,11 @@
 package base
 
-import "github.com/saime-0/nice-pea-chat/internal/domain"
-
-var _ domain.MembersRepository = (*MembersRepository)(nil)
+// TODO: раскомментить.
+// Эта строка будет выдавать ошибку во время компиляции,
+// если тип справа не имплементирует интерфейс слева
+//var _ domain.MembersRepository = (*MembersRepository)(nil)
 
 type MembersRepository struct {
 }
 
-func (m *MembersRepository) List(filter domain.MembersFilter) ([]domain.Member, error) {
-	return nil, nil
-}
-
-func (m *MembersRepository) Save(member domain.Member) error {
-	return nil
-}
-
-func (m *MembersRepository) Delete(id uint) error {
-	return nil
-}
+// TODO: реализовать методы
