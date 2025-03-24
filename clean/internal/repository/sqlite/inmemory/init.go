a872ecb3ca5025e4621a3a4de161eb3d1f8a0752
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-24 14:19:28 +0300

    добавить проверку директории и использование filepath

--- clean/internal/repository/sqlite/inmemory/init.go
+++ clean/internal/repository/sqlite/inmemory/init.go
@@ -3,6 +3,8 @@ package inmemory
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	_ "github.com/glebarez/go-sqlite"
 	"github.com/jmoiron/sqlx"
@@ -23,22 +25,31 @@ func Init(config Config) (*SQLiteInMemory, error) {
 	sqlin := &SQLiteInMemory{
 		db: db,
 	}
-	if err = migrate(sqlin.db, config); err != nil {
+	if err = migrate(sqlin.db, config.MigrationsDir); err != nil {
 		return nil, err
 	}
 
 	return sqlin, nil
 }
 
-func migrate(db *sqlx.DB, cfg Config) error {
-	entries, err := os.ReadDir(cfg.MigrationsDir)
+func migrate(db *sqlx.DB, migrationsDir string) error {
+	if migrationsDir == "" {
+		return nil
+	}
+	entries, err := os.ReadDir(migrationsDir)
 	if err != nil {
 		return fmt.Errorf("error reading migrations directory: %w", err)
 	}
 	for _, entry := range entries {
-		filename := cfg.MigrationsDir + string(os.PathSeparator) + entry.Name()
+		if entry.IsDir() {
+			continue
+		}
+		if !strings.HasSuffix(entry.Name(), ".up.sql") {
+			continue
+		}
+		path := filepath.Join(migrationsDir, entry.Name())
 		var file []byte
-		if file, err = os.ReadFile(filename); err != nil {
+		if file, err = os.ReadFile(path); err != nil {
 			return err
 		}
 		if _, err = db.Exec(string(file)); err != nil {
