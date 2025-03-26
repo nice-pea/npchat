48111d9f757576a4c70530004a3ba52f3ce6dd26
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-03-26 17:06:51 +0300

    Добавлен тест TestMember_ValidateID

--- clean/internal/domain/member.go
+++ clean/internal/domain/member.go
@@ -3,7 +3,11 @@ package domain
 type Member struct {
 	ID string
 	//UserID string
-	ChatID string 
+	ChatID string
+}
+
+func (m Member) ValidateID() error {
+	panic("unimplemented")
 }
 
 type MembersRepository interface {
