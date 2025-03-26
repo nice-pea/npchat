ba98b35e068ac32314c1c89348a77d7d2cf7afc8
Muhammed19m <71428103+Muhammed19m@users.noreply.github.com>
2025-03-26 16:46:10 +0300

    47816533/Добавить валидацию в модель invitations

--- clean/internal/domain/invitation.go
+++ clean/internal/domain/invitation.go
@@ -1,9 +1,36 @@
 package domain
 
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
 type Invitation struct {
 	ID string
 	// UserID string
 	ChatID string `db:"chat_id"`
+}
+
+var (
+	ErrInvitationIDValidate     = errors.New("некорректный UUID")
+	ErrInvitationChatIDValidate = errors.New("некорректный ChatID")
+)
+
+func (i Invitation) ValidateID() error {
+	if err := uuid.Validate(i.ID); err != nil {
+		return errors.Join(err, ErrInvitationIDValidate)
+	}
+
+	return nil
+}
+
+func (i Invitation) ValidateChatID() error {
+	if err := uuid.Validate(i.ChatID); err != nil {
+		return errors.Join(err, ErrInvitationChatIDValidate)
+	}
+
+	return nil
 }
 
 type InvitationsRepository interface {
@@ -14,5 +41,5 @@ type InvitationsRepository interface {
 
 type InvitationsFilter struct {
 	ID     string
-	ChatID string 
+	ChatID string
 }
