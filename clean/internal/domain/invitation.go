64c17f27e862c4eb8c46c6a91c1b11a1d453ba09
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-03-26 16:12:58 +0300

    Добавлен ValidateID

--- clean/internal/domain/invitation.go
+++ clean/internal/domain/invitation.go
@@ -1,9 +1,28 @@
 package domain
 
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
 type Invitation struct {
 	ID string
 	// UserID string
 	ChatID string `db:"chat_id"`
+}
+
+var (
+	ErrInvitationIDValidate     = errors.New("некорректный UUID")
+	ErrInvitationChatIDValidate = errors.New("некорректный ChatID")
+)
+
+func (i Invitation) ValidateID() error {
+	if err := uuid.Validate(i.ID); err != nil {
+		return errors.Join(err, ErrInvitationIDValidate)
+	}
+
+	return nil
 }
 
 type InvitationsRepository interface {
@@ -14,5 +33,5 @@ type InvitationsRepository interface {
 
 type InvitationsFilter struct {
 	ID     string
-	ChatID string 
+	ChatID string
 }
