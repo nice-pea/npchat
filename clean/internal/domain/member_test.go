98e491e36fdf307b3ad28b4fedf7387769456ff1
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-03-27 18:37:31 +0300

    адаптирован TestMember_ValidateChatID

--- clean/internal/domain/member_test.go
+++ clean/internal/domain/member_test.go
@@ -2,8 +2,6 @@ package domain
 
 import (
 	"testing"
-
-	"github.com/google/uuid"
 )
 
 func TestMember_ValidateID(t *testing.T) {
@@ -14,68 +12,8 @@ func TestMember_ValidateID(t *testing.T) {
 }
 
 func TestMember_ValidateChatID(t *testing.T) {
-	type fields struct {
-		ID     string
-		ChatID string
-	}
-
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		{
-			name:    "пустая строка",
-			fields:  fields{ChatID: ""},
-			wantErr: true,
-		},
-		{
-			name:    "коротка строка точно не uuid",
-			fields:  fields{ChatID: "fndsef"},
-			wantErr: true,
-		},
-		{
-			name:    "короткая строка из символов точно не uuid",
-			fields:  fields{ChatID: "----"},
-			wantErr: true,
-		},
-		{
-			name:    "строка из символов точно не uuid",
-			fields:  fields{ChatID: ",,,,,,,,"},
-			wantErr: true,
-		},
-		{
-			name:    "нужное количество символов",
-			fields:  fields{ChatID: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"},
-			wantErr: true,
-		},
-		{
-			name:    "это uuid",
-			fields:  fields{ChatID: "1cee9c74-a359-425c-b1bb-91c8a35e7b21"},
-			wantErr: false,
-		},
-		{
-			name:    "это uuid",
-			fields:  fields{ChatID: "0195ba16-f44c-7a3b-b326-94697ec6b00e"},
-			wantErr: false,
-		},
-		{
-			name:    "uuid генерируемый библиотекой",
-			fields:  fields{ChatID: uuid.NewString()},
-			wantErr: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			m := Member{
-				ID:     tt.fields.ID,
-				ChatID: tt.fields.ChatID,
-			}
-			if err := m.ValidateChatID(); (err != nil) != tt.wantErr {
-				t.Errorf("ValidateChatID() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-
+	RunValidateChatIDTest(t, func(ChatID string) error {
+		m := Member{ChatID: ChatID}
+		return m.ValidateChatID()
+	})
 }
