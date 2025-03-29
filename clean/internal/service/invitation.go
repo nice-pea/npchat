09212b18d96485cadd6222faf816419438a38a12
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-03-29 11:28:31 +0300

    fix

--- clean/internal/service/invitation.go
+++ clean/internal/service/invitation.go
@@ -18,7 +18,7 @@ var (
 	ErrChatInvitationsInputUserIDValidate = errors.New("некорректный UserID")
 	ErrChatInvitationsInputChatIDValidate = errors.New("некорректный ChatID")
 	ErrChatInvitationsNoChat              = errors.New("Не существует чата с данным ChatID")
-	ErrChatInvitationsUserIsNotChief      = errors.New("UserID не является ChiefUserID")
+	ErrChatInvitationsUserIsNotChief      = errors.New("доступно только для chief этого чата")
 )
 
 type ChatInvitationsInput struct {
@@ -38,7 +38,6 @@ func (in ChatInvitationsInput) Validate() error {
 
 // ChatInvitations - Получить список приглашений в конкретный чат
 func (i *Invitations) ChatInvitations(in ChatInvitationsInput) ([]domain.Invitation, error) {
-
 	if err := in.Validate(); err != nil {
 		return nil, err
 	}
