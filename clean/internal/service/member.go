526b797561b004e53d21b83a91ed56214ec9956e
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-30 18:46:40 +0300

    работающий код для списка участников

--- clean/internal/service/member.go
+++ clean/internal/service/member.go
@@ -1,1 +1,79 @@
 package service
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+
+	"github.com/saime-0/nice-pea-chat/internal/domain"
+)
+
+type Members struct {
+	MembersRepo domain.MembersRepository
+	ChatsRepo   domain.ChatsRepository
+}
+
+type ChatMembersInput struct {
+	ChatID        string
+	SubjectUserID string
+}
+
+var (
+	ErrChatMembersInputSubjectUserIDValidate = errors.New("некорректный SubjectUserID")
+	ErrChatMembersInputChatIDValidate        = errors.New("некорректный ChatID")
+)
+
+// Validate валидирует значение отдельно каждого параметры
+func (in ChatMembersInput) Validate() error {
+	if err := uuid.Validate(in.SubjectUserID); err != nil {
+		return errors.Join(err, ErrChatMembersInputSubjectUserIDValidate)
+	}
+	if err := uuid.Validate(in.ChatID); err != nil {
+		return errors.Join(err, ErrChatMembersInputChatIDValidate)
+	}
+
+	return nil
+}
+
+func (m *Members) ChatMembers(in ChatMembersInput) ([]domain.Member, error) {
+	// Валидировать параметры
+	var err error
+	if err = in.Validate(); err != nil {
+		return nil, err
+	}
+
+	// Проверить существование чата
+	chatsFilter := domain.ChatsFilter{
+		IDs: []string{in.ChatID},
+	}
+	chats, err := m.ChatsRepo.List(chatsFilter)
+	if err != nil {
+		return nil, err
+	}
+	if len(chats) != 1 {
+		return nil, errors.New("чата с таким ID не существует")
+	}
+
+	// Получить список участников
+	membersFilter := domain.MembersFilter{
+		ChatID: in.ChatID,
+	}
+	members, err := m.MembersRepo.List(membersFilter)
+	if err != nil {
+		return nil, err
+	}
+	if len(members) == 0 {
+		return nil, errors.New("пользователь не является участником чата")
+	}
+
+	// Проверить что пользователь является участником чата
+	for i, member := range members {
+		if member.UserID == in.SubjectUserID {
+			break
+		} else if i == len(members)-1 {
+			return nil, errors.New("пользователь не является участником чата")
+		}
+	}
+
+	return members, nil
+}
--- clean/internal/service/member_test.go
+++ clean/internal/service/member_test.go
@@ -3,13 +3,16 @@ package service
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/saime-0/nice-pea-chat/internal/domain"
+	"github.com/saime-0/nice-pea-chat/internal/domain/helpers_tests"
 	"github.com/saime-0/nice-pea-chat/internal/repository/sqlite/memory"
 )
 
 // newChatsService создает объект сервиса Chats с sqlite/memory репозиториями
-func newMembersService(t *testing.T) *Chats {
+func newMembersService(t *testing.T) *Members {
 	sqLiteInMemory, err := memory.Init(memory.Config{MigrationsDir: "../../migrations/repository/sqlite/memory"})
 	assert.NoError(t, err)
 	chatsRepository, err := sqLiteInMemory.NewChatsRepository()
@@ -17,40 +20,140 @@ func newMembersService(t *testing.T) *Chats {
 	membersRepository, err := sqLiteInMemory.NewMembersRepository()
 	assert.NoError(t, err)
 	return &Members{
-		//ChatsRepo:   chatsRepository,
-		//MembersRepo: membersRepository,
-		//History:     HistoryDummy{},
+		ChatsRepo:   chatsRepository,
+		MembersRepo: membersRepository,
 	}
+}
+
+func assertEqualMembers(t *testing.T, expected, actual domain.Member) {
+	assert.Equal(t, expected.ID, actual.ID)
+	assert.Equal(t, expected.UserID, actual.UserID)
+	assert.Equal(t, expected.ChatID, actual.ChatID)
 }
 
 /*
 Создать сервис service/Members и добавить ему несколько юзкейсов:
 Получить список участников чата
 
-	доступно только участникам чата
+	пользователь должен быть участникам чата
 	в список попадают все активные участники чата
 	Входящие параметры должны валидироваться
 
 Покинуть чат
 
-	недоступно для chief
-	доступно только участникам чата
+	пользователь должен быть участникам чата
+	пользователь должен не должен быть главным администратором
 	Входящие параметры должны валидироваться
 
 Принудительно удалить участника
 
-	доступно только для chief
+	пользователь должен быть участникам чата
+	пользователь должен быть главным администратором
 	нельзя удалить самого себя
 	Входящие параметры должны валидироваться
 */
 
-func Test_Members_ChatMembers(t *testing.T) {
+func Test_ChatMembersInput_Validate(t *testing.T) {
+	helpers_tests.RunValidateRequiredIDTest(t, func(id string) error {
+		in := ChatMembersInput{
+			ChatID:        id,
+			SubjectUserID: uuid.NewString(),
+		}
+		return in.Validate()
+	})
+	helpers_tests.RunValidateRequiredIDTest(t, func(id string) error {
+		in := ChatMembersInput{
+			ChatID:        uuid.NewString(),
+			SubjectUserID: id,
+		}
+		return in.Validate()
+	})
+}
 
+func Test_Members_ChatMembers(t *testing.T) {
+	t.Run("чат должен существовать", func(t *testing.T) {
+		membersService := newMembersService(t)
+		input := ChatMembersInput{
+			ChatID:        uuid.New().String(),
+			SubjectUserID: uuid.NewString(),
+		}
+		members, err := membersService.ChatMembers(input)
+		assert.Error(t, err)
+		assert.Len(t, members, 0)
+	})
+	t.Run("пользователь должен быть участником чата", func(t *testing.T) {
+		membersService := newMembersService(t)
+		chat := domain.Chat{ID: uuid.NewString()}
+		err := membersService.ChatsRepo.Save(chat)
+		assert.NoError(t, err)
+		member := domain.Member{
+			ID:     uuid.NewString(),
+			UserID: uuid.NewString(),
+			ChatID: uuid.NewString(),
+		}
+		err = membersService.MembersRepo.Save(member)
+		assert.NoError(t, err)
+		input := ChatMembersInput{
+			ChatID:        chat.ID,
+			SubjectUserID: member.UserID,
+		}
+		members, err := membersService.ChatMembers(input)
+		assert.Error(t, err)
+		assert.Len(t, members, 0)
+	})
+	t.Run("возвращается список участников чата", func(t *testing.T) {
+		membersService := newMembersService(t)
+		chat := domain.Chat{ID: uuid.NewString()}
+		err := membersService.ChatsRepo.Save(chat)
+		assert.NoError(t, err)
+		members := make([]domain.Member, 20)
+		for i := range members {
+			members[i] = domain.Member{
+				ID:     uuid.NewString(),
+				UserID: uuid.NewString(),
+				ChatID: chat.ID,
+			}
+			err = membersService.MembersRepo.Save(members[i])
+			assert.NoError(t, err)
+		}
+		subjectMember := domain.Member{
+			ID:     uuid.NewString(),
+			UserID: uuid.NewString(),
+			ChatID: chat.ID,
+		}
+		err = membersService.MembersRepo.Save(subjectMember)
+		assert.NoError(t, err)
+		input := ChatMembersInput{
+			ChatID:        subjectMember.ChatID,
+			SubjectUserID: subjectMember.UserID,
+		}
+		membersFromRepo, err := membersService.ChatMembers(input)
+		assert.NoError(t, err)
+		if assert.Len(t, membersFromRepo, len(members)+1) {
+			for i := range members {
+				assertEqualMembers(t, members[i], membersFromRepo[i])
+			}
+			lastRepoMember := membersFromRepo[len(membersFromRepo)-1]
+			assertEqualMembers(t, subjectMember, lastRepoMember)
+		}
+	})
 }
 
 func Test_Members_Leave(t *testing.T) {
-
+	_ = []string{
+		"чат должен существовать",
+		"пользователь должен быть участником чата",
+		"пользователь не должен быть главным администратором чата",
+		"после выхода пользователь перестает быть участником",
+		"",
+	}
 }
 func Test_Members_Delete(t *testing.T) {
-
+	_ = []string{
+		"чат должен существовать",
+		"пользователь должен быть участником чата",
+		"пользователь должен быть главным администратором чата",
+		"удаляемый участник должен существовать",
+		"",
+	}
 }
