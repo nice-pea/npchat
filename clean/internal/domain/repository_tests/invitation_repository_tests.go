cba39abef642d9043103bc9e964ce385a6549a16
Muhammed19m <71428103+Muhammed19m@users.noreply.github.com>
2025-04-01 20:08:20 +0300

    48060983 Добавить в приглашение SubjectUserID (#19)

--- clean/internal/domain/repository_tests/invitation_repository_tests.go
+++ clean/internal/domain/repository_tests/invitation_repository_tests.go
@@ -10,8 +10,14 @@ import (
 	"github.com/saime-0/nice-pea-chat/internal/domain"
 )
 
+func assertEqualInvitation(t *testing.T, expected, actual domain.Invitation) {
+	assert.Equal(t, expected.ID, actual.ID)
+	assert.Equal(t, expected.ChatID, actual.ChatID)
+	assert.Equal(t, expected.UserID, actual.UserID)
+	assert.Equal(t, expected.SubjectUserID, actual.SubjectUserID)
+}
+
 func InvitationsRepositoryTests(t *testing.T, newRepository func() domain.InvitationsRepository) {
-	t.Helper()
 	t.Run("List", func(t *testing.T) {
 		t.Run("без фильтра в пустом репозитории", func(t *testing.T) {
 			r := newRepository()
@@ -76,13 +82,13 @@ func InvitationsRepositoryTests(t *testing.T, newRepository func() domain.Invita
 			userID := uuid.NewString()
 			invitations := make([]domain.Invitation, amountInvs)
 			for i := range amountInvs {
-				inv := domain.Invitation{ID: uuid.NewString(), ChatID: uuid.NewString(), UserID: userID}
+				inv := domain.Invitation{ID: uuid.NewString(), UserID: userID}
 				invitations[i] = inv
 				err := r.Save(inv)
 				assert.NoError(t, err)
 			}
 			for range amountInvs {
-				inv := domain.Invitation{ID: uuid.NewString(), ChatID: uuid.NewString(), UserID: uuid.NewString()}
+				inv := domain.Invitation{ID: uuid.NewString()}
 				err := r.Save(inv)
 				assert.NoError(t, err)
 			}
@@ -90,9 +96,31 @@ func InvitationsRepositoryTests(t *testing.T, newRepository func() domain.Invita
 			assert.NoError(t, err)
 			if assert.Len(t, invitationsFromRepo, amountInvs) {
 				for i, inv := range invitationsFromRepo {
-					assert.Equal(t, inv.ID, invitations[i].ID)
-					assert.Equal(t, inv.ChatID, invitations[i].ChatID)
-					assert.Equal(t, inv.UserID, invitations[i].UserID)
+					assertEqualInvitation(t, inv, invitations[i])
+				}
+			}
+		})
+		t.Run("с фильтром по subject user id", func(t *testing.T) {
+			r := newRepository()
+			const amountInvs = 2
+			subjectUserID := uuid.NewString()
+			invitations := make([]domain.Invitation, amountInvs)
+			for i := range amountInvs {
+				inv := domain.Invitation{ID: uuid.NewString(), SubjectUserID: subjectUserID}
+				invitations[i] = inv
+				err := r.Save(inv)
+				assert.NoError(t, err)
+			}
+			for range amountInvs {
+				inv := domain.Invitation{ID: uuid.NewString()}
+				err := r.Save(inv)
+				assert.NoError(t, err)
+			}
+			invitationsFromRepo, err := r.List(domain.InvitationsFilter{SubjectUserID: subjectUserID})
+			assert.NoError(t, err)
+			if assert.Len(t, invitationsFromRepo, amountInvs) {
+				for i, inv := range invitationsFromRepo {
+					assertEqualInvitation(t, inv, invitations[i])
 				}
 			}
 		})
@@ -104,6 +132,14 @@ func InvitationsRepositoryTests(t *testing.T, newRepository func() domain.Invita
 				ID: "",
 			})
 			assert.Error(t, err)
+		})
+		t.Run("чат без subject user id", func(t *testing.T) {
+			r := newRepository()
+			err := r.Save(domain.Invitation{
+				ID:            uuid.NewString(),
+				SubjectUserID: "",
+			})
+			assert.NoError(t, err)
 		})
 		t.Run("чат без name", func(t *testing.T) {
 			r := newRepository()
@@ -139,6 +175,20 @@ func InvitationsRepositoryTests(t *testing.T, newRepository func() domain.Invita
 			assert.NoError(t, err)
 			err = r.Save(domain.Invitation{
 				ID: uuid.NewString(),
+			})
+			assert.NoError(t, err)
+		})
+		t.Run("дубль subject user id", func(t *testing.T) {
+			r := newRepository()
+			subjectUserId := uuid.NewString()
+			err := r.Save(domain.Invitation{
+				ID:            uuid.NewString(),
+				SubjectUserID: subjectUserId,
+			})
+			assert.NoError(t, err)
+			err = r.Save(domain.Invitation{
+				ID:            uuid.NewString(),
+				SubjectUserID: subjectUserId,
 			})
 			assert.NoError(t, err)
 		})
