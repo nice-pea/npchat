6a587c9c2b8854fcd8131adb1a2216f5be3e167b
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-04-01 16:17:38 +0300

    добавить в domain/invitation.go SubjectUserID

--- clean/internal/domain/invitation.go
+++ clean/internal/domain/invitation.go
@@ -7,15 +7,17 @@ import (
 )
 
 type Invitation struct {
-	ID     string
-	UserID string
-	ChatID string
+	ID            string
+	SubjectUserID string
+	UserID        string
+	ChatID        string
 }
 
 var (
-	ErrInvitationIDValidate     = errors.New("некорректный UUID")
-	ErrInvitationChatIDValidate = errors.New("некорректный ChatID")
-	ErrInvitationUserIDValidate = errors.New("некорректный UserID")
+	ErrInvitationIDValidate            = errors.New("некорректный UUID")
+	ErrInvitationChatIDValidate        = errors.New("некорректный ChatID")
+	ErrInvitationUserIDValidate        = errors.New("некорректный UserID")
+	ErrInvitationSubjectUserIDValidate = errors.New("некорректный SubjectUserID")
 )
 
 func (i Invitation) ValidateID() error {
@@ -42,6 +44,14 @@ func (i Invitation) ValidateUserID() error {
 	return nil
 }
 
+func (i Invitation) ValidateSubjectUserID() error {
+	if err := uuid.Validate(i.SubjectUserID); err != nil {
+		return errors.Join(err, ErrInvitationSubjectUserIDValidate)
+	}
+
+	return nil
+}
+
 type InvitationsRepository interface {
 	List(filter InvitationsFilter) ([]Invitation, error)
 	Save(invitation Invitation) error
@@ -49,7 +59,8 @@ type InvitationsRepository interface {
 }
 
 type InvitationsFilter struct {
-	ID     string
-	ChatID string
-	UserID string
+	ID            string
+	ChatID        string
+	UserID        string
+	SubjectUserID string
 }
