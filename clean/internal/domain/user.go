d534f07d48127ed4654d1dbde01073d48400d678
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-04-02 13:56:28 +0300

    добавить модель user

---
+++ clean/internal/domain/user.go
@@ -0,0 +1,34 @@
+package domain
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID string
+	// Name string
+}
+
+var (
+	ErrUserIDValidate = errors.New("некорректный UUID")
+)
+
+func (u User) ValidateID() error {
+	if err := uuid.Validate(u.ID); err != nil {
+		return errors.Join(err, ErrUserIDValidate)
+	}
+
+	return nil
+}
+
+type UsersRepository interface {
+	List(filter UsersFilter) ([]User, error)
+	Save(member User) error
+	Delete(id string) error
+}
+
+type UsersFilter struct {
+	ID string
+}
