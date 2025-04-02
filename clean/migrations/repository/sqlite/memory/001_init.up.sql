b986e37a009176b75e2b8a3fbecd2e6d8b21f66e
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-04-02 18:52:30 +0300

    в таблицу chats тоже добавить связь с таблицей users

--- clean/migrations/repository/sqlite/memory/001_init.up.sql
+++ clean/migrations/repository/sqlite/memory/001_init.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE chats
     id   TEXT PRIMARY KEY,
     name TEXT NOT NULL,
     chief_user_id TEXT NOT NULL
+    FOREIGN KEY (chief_user_id) REFERENCES users ON DELETE RESTRICT
 );
 
 CREATE TABLE members
