eb2c13ec3fef3fca09aab21692b4d801718bccac
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-04-02 20:48:47 +0300

    добавить функции

--- clean/internal/service/functions.go
+++ clean/internal/service/functions.go
@@ -1,1 +1,66 @@
 package service
+
+import (
+	"github.com/saime-0/nice-pea-chat/internal/domain"
+)
+
+type Repo interface {
+	Members
+}
+
+// chat возвращает чат либо ошибку ErrChatNotExists
+func chat(chatsRepo domain.ChatsRepository, chatID string) (domain.Chat, error) {
+	chatsFilter := domain.ChatsFilter{
+		IDs: []string{chatID},
+	}
+	chats, err := chatsRepo.List(chatsFilter)
+	if err != nil {
+		return domain.Chat{}, err
+	}
+	if len(chats) != 1 {
+		return domain.Chat{}, ErrChatNotExists
+	}
+
+	return chats[0], nil
+}
+
+// Получить список участников
+func chatMembers(membersRepo domain.MembersRepository, chatID string) ([]domain.Member, error) {
+	membersFilter := domain.MembersFilter{
+		ChatID: chatID,
+	}
+	members, err := membersRepo.List(membersFilter)
+	if err != nil {
+		return nil, err
+	}
+
+	return members, nil
+}
+
+// UserMember вернет участника либо ошибку ErrUserIsNotMember
+func userMember(membersRepo domain.MembersRepository, userID, chatID string) (domain.Member, error) {
+	return memberOrErr(membersRepo, userID, chatID, ErrUserIsNotMember)
+}
+
+// subjectUserMember вернет участника либо ошибку ErrSubjectUserIsNotMember
+func subjectUserMember(membersRepo domain.MembersRepository, subjectUserID, chatID string) (domain.Member, error) {
+	return memberOrErr(membersRepo, subjectUserID, chatID, ErrSubjectUserIsNotMember)
+}
+
+// memberOrErr возвращает участника чата по userID, chatID.
+// Вернет errOnNotExists ошибку если участника не будет существовать.
+func memberOrErr(membersRepo domain.MembersRepository, userID, chatID string, errOnNotExists error) (domain.Member, error) {
+	membersFilter := domain.MembersFilter{
+		UserID: userID,
+		ChatID: chatID,
+	}
+	members, err := membersRepo.List(membersFilter)
+	if err != nil {
+		return domain.Member{}, err
+	}
+	if len(members) != 1 {
+		return domain.Member{}, errOnNotExists
+	}
+
+	return members[0], nil
+}
