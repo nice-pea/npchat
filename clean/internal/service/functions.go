c3df2c3275cd39baa9c1a9a972918737c2c62d8c
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-04-03 12:59:41 +0300

    добавить функцию проверки существоания пользователя

--- clean/internal/service/functions.go
+++ clean/internal/service/functions.go
@@ -64,3 +64,16 @@ func memberOrErr(membersRepo domain.MembersRepository, userID, chatID string, er
 
 	return members[0], nil
 }
+
+func userOrErr(usersRepo domain.UsersRepository, id string) (domain.User, error) {
+	users, err := usersRepo.List(domain.UsersFilter{
+		ID: id,
+	})
+	if err != nil {
+		return domain.User{}, err
+	}
+	if len(users) != 1 {
+		return domain.User{}, ErrUserNotExists
+	}
+	return users[1], nil
+}
