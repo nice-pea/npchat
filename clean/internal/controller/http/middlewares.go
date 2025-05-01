3acaacb66deaa269b5129f2e13d1cbcc2ffc8124
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-05-01 09:34:05 +0300

    Добавить комментарии в mw
    Introduce error messages for authorization, request ID, and content type validation to improve clarity. Add comments for middleware functions to describe their responsibilities, enhancing code readability and maintainability.

--- clean/internal/controller/http/middlewares.go
+++ clean/internal/controller/http/middlewares.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 )
 
+// ErrUnauthorized запрос не содержит действительный токен авторизации
 var ErrUnauthorized = errors.New("unauthorized. Please, use token in header: Authorization: Bearer <token>")
 
+// requireAuthorizedSession требует авторизованную сессии
 func requireAuthorizedSession(next HandlerFunc) HandlerFunc {
 	return func(context Context) (any, error) {
 		if context.session.ID == "" {
@@ -16,8 +18,10 @@ func requireAuthorizedSession(next HandlerFunc) HandlerFunc {
 	}
 }
 
+// ErrUnknownRequestID запрос не содержит идентификатор запроса
 var ErrUnknownRequestID = errors.New("unknown request ID. Please, use X-Request-ID header")
 
+// requireRequestID требует наличие идентификатора запроса
 func requireRequestID(next HandlerFunc) HandlerFunc {
 	return func(context Context) (any, error) {
 		if context.requestID == "" {
@@ -28,8 +32,10 @@ func requireRequestID(next HandlerFunc) HandlerFunc {
 	}
 }
 
+// ErrUnsupportedAcceptedContentType клиент не принимает JSON в качестве ответа
 var ErrUnsupportedAcceptedContentType = errors.New("unsupported Accept header value. Please, use Accept: application/json header")
 
+// requireAcceptJson требует поддержку json как типа контента, который ожидание клиент
 func requireAcceptJson(next HandlerFunc) HandlerFunc {
 	return func(context Context) (any, error) {
 		if context.request.Header.Get("Accept") != "application/json" {
