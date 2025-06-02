b6e84639a7f245dcb99f0b57e1d56d8a30482c60
Saime <acc.saime.d@gmail.com>
2025-06-03 01:53:31 +0300

    изменить агрегат пользователя и сессии

--- internal/domain/aggregates.go
+++ internal/domain/aggregates.go
@@ -4,9 +4,16 @@ import (
 	"time"
 )
 
-// Чат
-
 // Пользователь
+
+type UserAggregate struct {
+	ID   string // ID пользователя
+	Name string // Имя пользователя
+	Nick string // Ник пользователя
+
+	BasicAuth     BasicAuth
+	OpenAuthLinks []OpenAuthLink
+}
 
 type BasicAuth struct {
 	Login    string // Логин пользователя
@@ -27,20 +34,46 @@ type OpenAuthToken struct {
 	Expiry       time.Time
 }
 
-type UserAggregate struct {
-	ID   string // ID пользователя
-	Name string // Имя пользователя
-	Nick string // Ник пользователя
-
-	BasicAuth     BasicAuth
-	OpenAuthLinks []OpenAuthLink
-	Sessions      []Session2
-}
+// Сессия
 
 type Session2 struct {
-	ID     string // ID сессии
-	Status int    // Статус сессии
+	ID           string // ID сессии
+	UserID       string
+	Name         string
+	Status       int // Статус сессии
+	AccessToken  SessionToken
+	RefreshToken SessionToken
+
+	//Client SessionClient
 }
+
+type SessionToken struct {
+	Token  string
+	Expiry time.Time
+}
+
+//type SessionClient struct {
+//	ID   string // Идентификатор устройства
+//	Type string // android/ios/web
+//}
+
+// Push-уведомления
+//
+//type PushConfig struct {
+//	UserID   string
+//	Channels []PushChannel // Имена каналов, на которые отправляются уведомления
+//}
+//
+//type PushChannel struct {
+//	Name     string       // Имя канала
+//	Provider PushProvider // ZeroValue - для глобального значения
+//	Enabled  bool         // Включено ли уведомление в канале
+//}
+//
+//type PushProvider struct {
+//	Name  string // apn, fcm, gcm, sms
+//	Token string // Токен для отправки уведомлений
+//}
 
 // Сообщение
 //
