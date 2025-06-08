2003ed29cfa29e5611f22f753881432ce2e8d48c
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-06-09 00:46:37 +0300

    перенести тест репозитория chatt в отдельный пакет

---
+++ internal/repository/sqlite/chatt_repository_test.go
@@ -0,0 +1,22 @@
+package sqlite
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/saime-0/nice-pea-chat/internal/domain/chatt"
+	chattRepoTests "github.com/saime-0/nice-pea-chat/internal/domain/chatt/repository_tests"
+)
+
+func TestChattRepository(t *testing.T) {
+	chattRepoTests.TestRepository(t, func() chatt.Repository {
+		repositoryFactory, err := InitRepositoryFactory(defaultTestConfig)
+		assert.Nil(t, err)
+		assert.NotNil(t, repositoryFactory)
+		repo := repositoryFactory.NewChatsRepository()
+		require.NotNil(t, repo)
+		return repo
+	})
+}
