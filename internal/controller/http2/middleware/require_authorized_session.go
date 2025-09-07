e7f3c7e980b4f64605a546f6808fa03e47826b1d
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-09-07 18:06:19 +0300

    изменить реализацию

--- internal/controller/http2/middleware/require_authorized_session.go
+++ internal/controller/http2/middleware/require_authorized_session.go
@@ -11,28 +11,39 @@ import (
 const CtxKeyUserSession = "userSession"
 
 // RequireAuthorizedSession требует авторизованную сессии
-func RequireAuthorizedSession(uc UsecasesForRequireAuthorizedSession) fiber.Handler {
+func RequireAuthorizedSession(uc UsecasesForRequireAuthorizedSession, tm UsecasesForRequireAuthorizedParseJWT) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		// Прочитать заголовок
 		header := ctx.Get("Authorization")
-		token, found := strings.CutPrefix(header, "Bearer ")
-		if token == "" || !found {
+
+		parts := strings.Split(header, " ")
+		if len(parts) != 2 {
 			return fiber.ErrUnauthorized
 		}
 
-		// Найти сессию по токену
-		out, err := uc.FindSessions(findSession.In{
-			Token: token,
-		})
-		if err != nil {
-			return fiber.ErrInternalServerError
-		}
-		if len(out.Sessions) != 1 {
+		authType := parts[0]
+		token := parts[1]
+
+		switch authType {
+		case "Bearer":
+			out, err := bearer(uc, token)
+			if err != nil {
+				return err
+			}
+			// Сохранить сессию в контекст
+			ctx.Locals(CtxKeyUserSession, out.Sessions[0])
+
+		case "JWT":
+			out, err := parseJwt(tm, token)
+			if err != nil {
+				return err
+			}
+			ctx.Locals("UserID", out.UserID)
+			ctx.Locals("SessionID", out.SessionID)
+
+		default:
 			return fiber.ErrUnauthorized
 		}
-
-		// Сохранить сессию в контекст
-		ctx.Locals(CtxKeyUserSession, out.Sessions[0])
 
 		return ctx.Next()
 	}
@@ -41,4 +52,37 @@ func RequireAuthorizedSession(uc UsecasesForRequireAuthorizedSession) fiber.Hand
 // UsecasesForRequireAuthorizedSession определяет интерфейс для доступа к сценариям использования бизнес-логики
 type UsecasesForRequireAuthorizedSession interface {
 	FindSessions(findSession.In) (findSession.Out, error)
+}
+
+func bearer(uc UsecasesForRequireAuthorizedSession, token string) (findSession.Out, error) {
+	// Найти сессию по токену
+	out, err := uc.FindSessions(findSession.In{
+		Token: token,
+	})
+	if err != nil {
+		return findSession.Out{}, fiber.ErrInternalServerError
+	}
+	if len(out.Sessions) != 1 {
+		return findSession.Out{}, fiber.ErrUnauthorized
+	}
+
+	return out, nil
+}
+
+type OutJWT struct {
+	UserID    string
+	SessionID string
+}
+
+type UsecasesForRequireAuthorizedParseJWT interface {
+	Parse(token string) (OutJWT, error)
+}
+
+func parseJwt(ucjwt UsecasesForRequireAuthorizedParseJWT, token string) (OutJWT, error) {
+	out, err := ucjwt.Parse(token)
+	if err != nil {
+		return OutJWT{}, fiber.ErrUnauthorized
+	}
+
+	return out, nil
 }
