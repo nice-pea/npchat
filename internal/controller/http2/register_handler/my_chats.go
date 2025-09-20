08af478ca753f947498a76c94c681732a4bba011
Saime <acc.saime.d@gmail.com>
2025-09-20 20:41:38 +0300

    Добавить поле page token в http обработчике (#101)

--- internal/controller/http2/register_handler/my_chats.go
+++ internal/controller/http2/register_handler/my_chats.go
@@ -1,6 +1,10 @@
 package register_handler
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	recover2 "github.com/gofiber/fiber/v2/middleware/recover"
 
@@ -17,18 +21,31 @@ func MyChats(router *fiber.App, uc UsecasesForMyChats, jparser middleware.JwtPar
 		"/chats",
 		recover2.New(),
 		middleware.RequireAuthorizedSession(uc, jparser),
-		func(context *fiber.Ctx) error {
+		func(ctx *fiber.Ctx) error {
+			keyset, err := decodeKeyset(ctx.Query("page_token"))
+			if err != nil {
+				return err
+			}
+
 			input := myChats.In{
-				SubjectID: UserID(context),
-				UserID:    UserID(context),
+				SubjectID: UserID(ctx),
+				UserID:    UserID(ctx),
+				Keyset:    keyset,
 			}
 
 			out, err := uc.MyChats(input)
 			if err != nil {
 				return err
 			}
+			nextPageToken, err := encodeKeyset(out.NextKeyset)
+			if err != nil {
+				return err
+			}
 
-			return context.JSON(out)
+			return ctx.JSON(fiber.Map{
+				"Chats":           out.Chats,
+				"next_page_token": nextPageToken,
+			})
 		},
 	)
 }
@@ -37,4 +54,30 @@ func MyChats(router *fiber.App, uc UsecasesForMyChats, jparser middleware.JwtPar
 type UsecasesForMyChats interface {
 	MyChats(myChats.In) (myChats.Out, error)
 	middleware.UsecasesForRequireAuthorizedSession
+}
+
+// decodeKeyset расшифровывает строку в формате base64 и разбирает ее на Keyset
+func decodeKeyset(pageToken string) (myChats.Keyset, error) {
+	if pageToken == "" {
+		return myChats.Keyset{}, nil
+	}
+	b, err := base64.StdEncoding.DecodeString(pageToken)
+	if err != nil {
+		return myChats.Keyset{}, fmt.Errorf("decode page_token: %w", err)
+	}
+	var keyset myChats.Keyset
+	return keyset, json.Unmarshal(b, &keyset)
+}
+
+// encodeKeyset преобразует keyset в json и кодирует в строку в base64
+func encodeKeyset(keyset myChats.Keyset) (string, error) {
+	if keyset == (myChats.Keyset{}) {
+		return "", nil
+	}
+	b, err := json.Marshal(keyset)
+	if err != nil {
+		return "", fmt.Errorf("marshal keyset: %w", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(b), nil
 }
--- internal/usecases/chats/my_chats/usecase_test.go
+++ internal/usecases/chats/my_chats/usecase_test.go
@@ -2,10 +2,12 @@ package myChats
 
 import (
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	testifySuite "github.com/stretchr/testify/suite"
 
+	"github.com/nice-pea/npchat/internal/domain/chatt"
 	serviceSuite "github.com/nice-pea/npchat/internal/usecases/suite"
 )
 
@@ -30,14 +32,14 @@ func (suite *testSuite) Test_Chats_MyChats() {
 		}
 		out, err := usecase.MyChats(input)
 		suite.ErrorIs(err, ErrUnauthorizedChatsView)
-		suite.Empty(out)
+		suite.Empty(out.Chats)
 	})
 
 	suite.Run("пустой список из пустого репозитория", func() {
 		input := suite.newUserChatsInput(uuid.New())
 		out, err := usecase.MyChats(input)
 		suite.NoError(err)
-		suite.Empty(out)
+		suite.Empty(out.Chats)
 	})
 
 	suite.Run("пустой список если у пользователя нет чатов", func() {
@@ -49,7 +51,7 @@ func (suite *testSuite) Test_Chats_MyChats() {
 		input := suite.newUserChatsInput(uuid.New())
 		out, err := usecase.MyChats(input)
 		suite.NoError(err)
-		suite.Empty(out)
+		suite.Empty(out.Chats)
 	})
 
 	suite.Run("у пользователя может быть несколько чатов", func() {
@@ -65,6 +67,59 @@ func (suite *testSuite) Test_Chats_MyChats() {
 		out, err := usecase.MyChats(input)
 		suite.NoError(err)
 		suite.Len(out.Chats, chatsAllCount)
+	})
+
+	suite.Run("возвращает NextKeyset пустым если осталось чатов меньше чем page", func() {
+		userID := uuid.New()
+		const chatsAllCount = 11
+		for range chatsAllCount {
+			chat := suite.RndChat()
+			p := suite.NewParticipant(userID)
+			suite.AddParticipant(&chat, p)
+			suite.UpsertChat(chat)
+		}
+		input := suite.newUserChatsInput(userID)
+		out, err := usecase.MyChats(input)
+		suite.NoError(err)
+		suite.Zero(out.NextKeyset)
+	})
+
+	suite.Run("с установленным Keyset вернется только часть чатов", func() {
+		userID := uuid.New()
+		//const chatsAllCount = 11
+		now := time.Now()
+		activeBefore := now.Add(-time.Hour)
+		// Список чатов, которые должны вернуться в результате
+		expectedChats := make([]chatt.Chat, 10)
+		for i := range expectedChats {
+			chat := suite.RndChat()
+			chat.LastActiveAt = activeBefore.
+				Add(-time.Duration(i+1) * time.Minute).
+				UTC().
+				Truncate(time.Millisecond)
+			suite.AddParticipant(&chat, suite.NewParticipant(userID))
+			expectedChats[i] = suite.UpsertChat(chat)
+		}
+		// Чаты, которые не должны вернуться
+		for range 11 {
+			chat := suite.RndChat()
+			suite.AddParticipant(&chat, suite.NewParticipant(userID))
+			suite.UpsertChat(chat)
+		}
+		// Получить список чатов
+		out, err := usecase.MyChats(In{
+			SubjectID: userID,
+			UserID:    userID,
+			Keyset: Keyset{
+				ActiveBefore: activeBefore,
+			},
+		})
+		suite.NoError(err)
+		suite.Zero(out.NextKeyset)
+		suite.Require().Len(out.Chats, len(expectedChats))
+		for i, chat := range out.Chats {
+			suite.Equal(expectedChats[i], chat)
+		}
 	})
 }
 
