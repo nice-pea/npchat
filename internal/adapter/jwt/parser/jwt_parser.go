ea6d8f89848989d718dfdefd8cba18a4962b85ef
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-09-25 00:45:49 +0300

    добавить продвинутый парсер ValidateJWTWithInvalidation

--- internal/adapter/jwt/parser/jwt_parser.go
+++ internal/adapter/jwt/parser/jwt_parser.go
@@ -6,8 +6,12 @@ import (
 	"time"
 
 	"github.com/cristalhq/jwt/v5"
+	"github.com/google/uuid"
 
 	jwt2 "github.com/nice-pea/npchat/internal/adapter/jwt"
+
+	redisCache "github.com/nice-pea/npchat/internal/adapter/jwt/repository/redis"
+
 	"github.com/nice-pea/npchat/internal/controller/http2/middleware"
 )
 
@@ -18,10 +22,12 @@ type OutJWT struct {
 
 type Parser struct {
 	Config jwt2.Config
+	cache  redisCache.JWTIssuanceRegistry
 }
 
 var (
-	ErrTimeOut = errors.New("время жизни токена истекло")
+	ErrTimeOut      = errors.New("время жизни токена истекло")
+	ErrTokenRevoked = errors.New("токен аннулирован")
 )
 
 type CustomClaims struct {
@@ -66,4 +72,55 @@ func (p *Parser) Parse(token string) (middleware.OutJwt, error) {
 		return middleware.OutJwt{}, ErrTimeOut
 	}
 	return customClaimsToOutJWT(newClaims), nil
+}
+
+func (p *Parser) ValidateJWTWithInvalidation(token string) (middleware.OutJwt, error) {
+
+	// create a Verifier (HMAC in this example)
+	verifier, err := jwt.NewVerifierHS(jwt.HS256, []byte(p.Config.SecretKey))
+
+	if err != nil {
+		return middleware.OutJwt{}, err
+	}
+
+	// parse and verify a token
+	tokenBytes := []byte(token)
+	newToken, err := jwt.Parse(tokenBytes, verifier)
+	if err != nil {
+		return middleware.OutJwt{}, err
+	}
+
+	err = verifier.Verify(newToken)
+	if err != nil {
+		return middleware.OutJwt{}, err
+	}
+	// get Registered claims
+	var newClaims CustomClaims
+	errClaims := json.Unmarshal(newToken.Claims(), &newClaims)
+	if errClaims != nil {
+		return middleware.OutJwt{}, err
+	}
+	// verify claims as you wish
+	if !newClaims.IsValidAt(time.Now()) {
+		return middleware.OutJwt{}, ErrTimeOut
+	}
+
+	sessionId, err := uuid.Parse(newClaims.SessionID)
+	if err != nil {
+		return middleware.OutJwt{}, err
+	}
+
+	issuedAt := newClaims.IssuedAt
+
+	timefromCache, err := p.cache.GetIssueTime(sessionId)
+	if err != nil {
+		return middleware.OutJwt{}, err
+	}
+
+	if timefromCache.After(issuedAt.Time) {
+		return middleware.OutJwt{}, ErrTokenRevoked
+	}
+
+	return customClaimsToOutJWT(newClaims), nil
+
 }
