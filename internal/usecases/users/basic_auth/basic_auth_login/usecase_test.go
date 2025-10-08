49935d3b800d10f7911663922dd649320fc1568c
Muhammed19m <muhammedmustafaev55@gmail.com>
2025-10-08 21:26:47 +0300

    Добавить тесты basic_auth_login с моками

--- internal/usecases/users/basic_auth/basic_auth_login/usecase_test.go
+++ internal/usecases/users/basic_auth/basic_auth_login/usecase_test.go
@@ -3,14 +3,16 @@ package basicAuthLogin
 import (
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	testifySuite "github.com/stretchr/testify/suite"
 
 	"github.com/nice-pea/npchat/internal/domain/sessionn"
+	"github.com/nice-pea/npchat/internal/domain/userr"
 	serviceSuite "github.com/nice-pea/npchat/internal/usecases/suite"
 )
 
 type testSuite struct {
-	serviceSuite.Suite
+	serviceSuite.SuiteWithMocks
 }
 
 func Test_TestSuite(t *testing.T) {
@@ -22,6 +24,8 @@ func (suite *testSuite) Test_BasicAuthLogin() {
 		Repo:         suite.RR.Users,
 		SessionsRepo: suite.RR.Sessions,
 	}
+	mockRepoUsers := suite.RR.Users
+	mockRepoSessions := suite.RR.Sessions
 
 	suite.Run("Login должен быть валидным", func() {
 		out, err := usecase.BasicAuthLogin(In{
@@ -42,6 +46,7 @@ func (suite *testSuite) Test_BasicAuthLogin() {
 	})
 
 	suite.Run("неверные данные", func() {
+		mockRepoUsers.EXPECT().List(mock.Anything).Return(nil, nil).Once()
 		out, err := usecase.BasicAuthLogin(In{
 			Login:    "wrongLogin",
 			Password: "wrongPassword123!",
@@ -53,25 +58,17 @@ func (suite *testSuite) Test_BasicAuthLogin() {
 	suite.Run("вернется Verified сессия", func() {
 		// Создаем нового пользователя с AuthnPassword
 		user := suite.NewRndUserWithBasicAuth()
-		err := suite.RR.Users.Upsert(user)
-		suite.Require().NoError(err)
 		// Входим сессию с правильными данными
 		input := In{
 			Login:    user.BasicAuth.Login,
 			Password: user.BasicAuth.Password,
 		}
+		mockRepoUsers.EXPECT().List(mock.Anything).Return([]userr.User{user}, nil).Once()
+		mockRepoSessions.EXPECT().Upsert(mock.Anything).Return(nil).Once()
 		output, err := usecase.BasicAuthLogin(input)
 		suite.NoError(err)
 		suite.Require().NotZero(output)
 		suite.Require().Equal(user.ID, output.Session.UserID)
 		suite.Require().Equal(sessionn.StatusVerified, output.Session.Status)
-
-		// Проверяем, что сессия сохранена в репозитории
-		sessions, err := suite.RR.Sessions.List(sessionn.Filter{
-			AccessToken: output.Session.AccessToken.Token,
-		})
-		suite.NoError(err)
-		suite.Require().Len(sessions, 1)
-		suite.EqualSessions(output.Session, sessions[0])
 	})
 }
